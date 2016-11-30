121dfd8d06eaad964c03e7df2cccb9fe822df0e2
Abby Tisdale <atisdale@hmc.edu>
2016-11-29 21:45:35 -0800

    Building business latent feature matrix.

--- bus_latent_features.py
+++ bus_latent_features.py
@@ -1,12 +1,13 @@
 import json
 from scipy import sparse, io
 import numpy as np
+import csv
 
-business_clean = open('test_clean.json', 'w+')
+#business_clean = open('test_clean.json', 'w+')
 business_dict = {}
 attributes = []
 
-for line in open('test.json'):
+for line in open('yelp_academic_dataset_business.json'):
 	business_json = json.loads(line)
 	if 'attributes' in business_json:
 		buss_attr = business_json["attributes"]
@@ -18,25 +19,41 @@ for line in open('test.json'):
 				attr_list.append((attr, buss_attr[attr]))
 		business_dict[business_json["business_id"]] = attr_list
 
-print(business_dict)
-print(attributes)
-modif_attr = [0]
+# print(business_dict)
+# print(attributes)
+modif_attr = ['Business ID']
 modif_attr.extend(attributes)
 big_array = np.array(modif_attr)
 
-print(big_array)
+# print(big_array)
 for bus in business_dict:
-	arr = [0]*len(modif_attr)
-	print(arr)
+	arr = [-1]*len(modif_attr)
+	# print(arr)
 	arr[0] = bus
 	for elem, truth in business_dict[bus]:
+		# print(truth)
 		arr[modif_attr.index(elem)] = int(truth)
+	big_array = np.vstack((big_array, arr))
+# print(big_array)
+
+rowNum, colNum = big_array.shape
+
+outputFile = open('businessLatentFt_data.csv','w+', newline='')
+outputFile1 = open('businessLatentFt_headers.csv','w+', newline='')
+outputFile2 = open('businessLatentFt_ids.csv','w+', newline='')
+csv_writer = csv.writer(outputFile)
+csv_writer1 = csv.writer(outputFile1)
+csv_writer2 = csv.writer(outputFile2)
+csv_writer1.writerow(big_array[0][1:])
 
 
-shape = (len(business_dict.keys()), len(attributes))
-matrix = sparse.coo_matrix(shape)
-print(matrix.shape)
+for i in range(1, rowNum):
+	csv_writer.writerow(big_array[i][1:])
+	csv_writer2.writerow([big_array[i][0]])
+
+outputFile.close()
+outputFile1.close()
+outputFile2.close()
 
 
-io.savemat('business_attr.mat', dict(matrix = matrix))
 
